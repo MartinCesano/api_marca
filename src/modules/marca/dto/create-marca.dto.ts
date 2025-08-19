c350363b4fdf349bc3081ead25e965ab60fe854c
MartinCesano <cesano305@gmail.com>
2025-08-19 02:53:57 +0000

    Actualizacion de la version de node, e instalación de la libreria class-validator

--- src/modules/marca/dto/create-marca.dto.ts
+++ src/modules/marca/dto/create-marca.dto.ts
@@ -1,3 +1,12 @@
+import { IsDate, IsOptional, IsString } from 'class-validator';
 export class CreateMarcaDto {
     readonly denominacion: string;
+
+    @IsOptional()
+    @IsString()
+    observaciones?: string;
+
+    @IsOptional()
+    @IsDate()
+    createdAt?: Date;
 }
