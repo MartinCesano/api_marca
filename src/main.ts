126856331959474e19399266420bc36db4b5ecd9
MartinCesano <cesano305@gmail.com>
2025-08-29 03:48:41 +0000

    Se habilitaron los cors para cualquier origen

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    
+    /*
+    Crea la aplicacion a partir del modulo principal AppModule
+    que es el que importa todos los demas modulos
+    */
     const app = await NestFactory.create(AppModule);
+    
+    /* 
+    Habilitar Cross-Origin Resource Sharing 
+    Sirve para permitir peticiones desde otros origenes, 
+    si no tiene especificacion permite cualquiera al especificar
+    se puede permitir solo de origenes en especifico
+     */
+    app.enableCors();
+
+    /*
+    Toma la configuracion de puerto desde el env
+    y si no esta definido setea por defecto el 3000
+    */
     await app.listen(process.env.PORT ?? 3000);
+    
 }
 bootstrap();
+
