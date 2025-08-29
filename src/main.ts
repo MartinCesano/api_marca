fa4537c8fe41ee2bb0e22380ea482a289093e3b7
MartinCesano <cesano305@gmail.com>
2025-08-29 04:05:49 +0000

    Cors habilitados correctamente con la ip de localhost en lugar de 'localhost'

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
      */
     app.enableCors(
         {
-            origin: 'http://localhost:5173',
+            origin: 'http://127.0.0.1:5173',
         }
     );
 
